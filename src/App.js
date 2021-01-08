a7405f7c8cc7d84235a609104b272d4d66e26de6
Clare Jackson <clare-jackson@lambdastudents.com>
2021-01-07 19:22:36 -0500

    Stretch Remove item done.

--- src/App.js
+++ src/App.js
@@ -19,10 +19,14 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = item => {
+		setCart(cart.filter((book) => item !== book.id))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 				<Navigation />
 
 			{/* Routes */}
